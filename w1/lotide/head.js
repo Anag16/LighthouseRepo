d7e2c9e34516c954c6a82241157908eda13dc59c
Ana Ruiz <ruizana393@gmail.com>
2021-08-23 14:02:48 -0400

    updated files

--- w1/lotide/head.js
+++ w1/lotide/head.js
@@ -1,12 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
-    
-  } else {
-    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+
+
 
 
 
@@ -17,8 +11,4 @@ const head = function(array) {
 
 
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-
+module.exports = head;
